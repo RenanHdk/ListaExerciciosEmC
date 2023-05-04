56878304103aa6c2e79f99fe19ade0788b46ba29
Renan <114091580+RenanHdk@users.noreply.github.com>
2023-05-04 10:40:13 -0300

    Update StructAluno.c

--- StructAluno.c
+++ StructAluno.c
@@ -12,43 +12,30 @@ Autor: Renan Hideki Sugiura
 
 typedef struct{
 	char nome[20];
-	int nota;
+	float nota;
 } ALUNO;
-
-int valid(char *txt, int opcao, int a, int b){
-	int tmp;
-	if(opcao==1){
-		printf(txt); scanf("%d", &tmp);
-		while(tmp<=a){
-			printf("Invalido, insira novamente: "); scanf("%d", &tmp);
-		}
-	}
-	else if(opcao==2){
-		printf(txt); scanf("%d", &tmp);
-		while(tmp<a || tmp>b){
-			printf("Invalido, insira novamente: "); scanf("%d", &tmp);
-		}
-	}
-	return tmp;
-}
 
 
 void entradaDdos(ALUNO *v, int qntd){
 	int i;
 	for(i=0; i<qntd; i++){
 		printf("\nNome do Aluno: "); scanf("%s", v[i].nome);
-		v[i].nota = valid("Nota do Aluno: ", 2, 0, 10);
+		printf("Nota do aluno: "); scanf("%f", &v[i].nota);
+		while(v[i].nota<0 || v[i].nota>10){
+			printf("Nota invalida, insira novamente: "); scanf("%f", &v[i].nota);
+		}
 	}
 }
 
 void print(ALUNO *v, int qntd){
 	int i;
 	for(i=0; i<qntd;i++)
-		printf("\nNome: %s\nNota: %d\n", v[i].nome, v[i].nota);
+		printf("\nNome: %s\nNota: %.2f\n", v[i].nome, v[i].nota);
 }
 
-int media(ALUNO *v, int qntd){
-	int media = 0, i;
+float media(ALUNO *v, int qntd){
+	float media = 0; 
+	int i;
 	for(i=0; i<qntd; i++){
 		media+=v[i].nota;
 	}
@@ -56,11 +43,15 @@ int media(ALUNO *v, int qntd){
 }
 
 int main(){
-	int qntd = valid("Insira a quantidade de alunos: ", 1, 0, 0);
-	int mediaAlunos;
+	int qntd;
+	printf("Insira a quantidade de alunos: "); scanf("%d", &qntd);
+	while(qntd<0){
+		printf("Quantidade invalida, insira novamente: "); scanf("%d", qntd);
+	}
+	float mediaAlunos;
 	ALUNO v[qntd];
 	entradaDdos(v, qntd);
 	print(v, qntd);
 	mediaAlunos = media(v, qntd);
-	printf("\n\nMedia aproximada da nota dos alunos: %d", mediaAlunos);
+	printf("\n\nMedia aproximada da nota dos alunos: %.2f", mediaAlunos);
 }
